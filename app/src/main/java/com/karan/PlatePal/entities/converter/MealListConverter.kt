dec04f093b4da610bcfc356eade17b0dcf1635ca
Karan <kc2532002@gmail.com>
2023-05-02 17:13:43 +0530

    first commit

---
+++ app/src/main/java/com/karan/PlatePal/entities/converter/MealListConverter.kt
@@ -0,0 +1,34 @@
+package com.karan.PlatePal.entities.converter
+
+import androidx.room.TypeConverter
+import com.karan.PlatePal.entities.MealsItems
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class MealListConverter {
+    @TypeConverter
+    fun fromCategoryList(category: List<MealsItems>):String?{
+        if (category == null){
+            return (null)
+        }else{
+            val gson = Gson()
+            val type = object : TypeToken<MealsItems>(){
+
+            }.type
+            return gson.toJson(category,type)
+        }
+    }
+
+    @TypeConverter
+    fun toCategoryList ( categoryString: String):List<MealsItems>?{
+        if (categoryString == null){
+            return (null)
+        }else{
+            val gson = Gson()
+            val type = object :TypeToken<MealsItems>(){
+
+            }.type
+            return  gson.fromJson(categoryString,type)
+        }
+    }
+}
---
+++ app/src/main/java/com/karan/PlatePal/interfaces/GetDataService.kt
@@ -0,0 +1,21 @@
+package com.karan.PlatePal.interfaces
+
+import com.karan.PlatePal.entities.Category
+import com.karan.PlatePal.entities.Meal
+import com.karan.PlatePal.entities.MealResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GetDataService {
+    @GET("categories.php")
+    fun getCategoryList(): Call<Category>
+
+    @GET("filter.php")
+    fun getMealList(@Query("c") category: String): Call<Meal>
+
+    @GET("lookup.php")
+    fun getSpecificItem(@Query("i") id: String): Call<MealResponse>
+
+
+}
